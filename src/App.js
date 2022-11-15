432b87ff1b9c5c23bca65a3428acbde950bcd102
Eitan Pushett <eitan.pushett@cnvrg.io>
2022-11-15 21:10:42 +0200

    added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-
 import './App.css';
+import Counter from './Counter';
+
 
 function App() {
   return (
     <div className="App">
       Usestate
       <p>Another line</p>
+    <Counter/>
     </div>
+
   );
 }
 
