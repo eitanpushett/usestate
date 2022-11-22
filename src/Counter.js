d3b266792169c4761ef6abc1d3393b67063d6c54
Eitan Pushett <eitan.pushett@cnvrg.io>
2022-11-22 20:34:30 +0200

    added delta option

--- src/Counter.js
+++ src/Counter.js
@@ -1,28 +1,23 @@
 import { useState } from "react";
 
 function Counter() {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(1)
+    const [delta, setDelta] = useState(1)
 
     function incr(){
         setCount(
             function(oldCount){
-                return oldCount + 1
+                return oldCount + delta
             }
             
         )
           }
     function reset(){
-
-      setCount(
-        function(oldCount){
-          oldCount = 0
-          return oldCount
+      setCount(0)
         }
-      )
-    }
+    
 
     function decr(){
-
       setCount(
         function(oldCount){
           return oldCount - 1
@@ -31,13 +26,19 @@ function Counter() {
     }
         //console.log(count) useful for debugging
     
+        function handleDelta(e){
+          console.log(e)
+          setDelta(Number(e.target.value))
+
+        }
 
     return (
       <div className="counter">
       <h1>React Counter</h1>
+      <input type = "number" value = {delta} onChange = {handleDelta} />
       <span className="counter__output">{count}</span>
       <div className="btn__container">
-        <button className="control__btn" onClick={incr}>+</button>
+        <button className="control__btn" onClick={incr}>Click to add {delta} to Counter</button>
         <button className="control__btn" onClick={decr}>-</button>
         <button className="reset" onClick={reset}>Reset</button>
       </div>
